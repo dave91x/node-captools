7c1877b4766033ce26387268ed0e8430091dd87c
Dave <daves@captricity.com>
2014-11-29 22:40:50 -0500

    start to fill in index.js and add tests to test.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
-var captools = {};
+var captools = {
+	
+	apiToken: '',
+	/**
+	  * @param   {String} token
+	  * @return  self Object
+	 */
+	connect: function(token) {
+		self.apiToken = token;
+		return self;
+	}
+};
 
 if (typeof module !== 'undefined') {
 	module.exports = captools;
--- test/test.js
+++ test/test.js
@@ -1,16 +1,24 @@
 // First, we require 'expect' from Chai.
-var chai     = require('chai');
-var expect   = chai.expect;
+var chai       = require('chai'),
+    should     = chai.should(),
+	captools   = require('../index.js'),
+	connect    = captools.connect;
+	
 
 // describe a suite (or group) of tests
 describe('captools test suite 1', function() {
 	
 	// The tests have an English description...
-	it('has 2 equal to or greater than 0', function() {
-		// and a code assertion.
-		expect(2).to.be.above(0);
-	});
+	// it('has 2 equal to or greater than 0', function() {
+	// 	// and a code assertion.
+	// 	expect(2).to.be.above(0);
+	// });
 	
+	it('connects with api token', function() {
+		captools.apiToken.should.equal('');
+		connect('myApiToken');
+		captools.apiToken.should.equal('myApiToken');
+	});
 });
 
 
