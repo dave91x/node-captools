ed7abb5b811bca337d2f8eb1ef0812f84cce680f
Dave <daves@captricity.com>
2015-06-27 06:03:45 -0400

    starting to add function calls for the more basic API endpoint calls

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
 var request = require('request');
-request.debug = true;
+request.debug = false;
 
 var captools = {
 	
-	//apiToken: '',
-	
-	headers: { 
+	headers: {
 	  'Captricity-API-Token': '',
 	  'User-Agent': 'node-captools [request]'
 	},
 	
 	/**
-	  * @param   {String} token
-	  * @return  this
+     * Make a way to set the Captricity-API-Token in the header once for subsequent calls
+     * @param   {String} token
+     * @return  this
 	 */
 	connect: function(token) {
 		this['headers']['Captricity-API-Token'] = token;
 		return this;
 	},
-	
+
+    /**
+     *
+     * @param url
+     * @param callback_function
+     */
+    //apiGetCall: function(url, callback_function) {
+    //    request({url: url, headers: this.headers}, function(error, response, body) {
+    //        // Check for error
+    //        if (!error && response.statusCode == 200) {
+    //            callback_function(body)
+    //        } else {
+    //            console.log('Status Code Returned:', response.statusCode);
+    //            console.log('Error:', error);
+    //        }
+    //    })
+    //},
+
 	/**
-	  * @param   {}
-	  * @return  []
+     * Get information on all Batches owned by calling user
+     * @param   {}
+     * @return  []
 	 */
 	listBatches: function() {
-		//console.log(this.headers);
-		var batches = request({url: 'https://shreddr.captricity.com/api/v1/batch/', headers: this.headers}, function(e,r,b) {console.log(b);});
-		// , function(e,r,b) {return r});
-		// , function(e,r,b) {console.log(r);});
-	 	// function(error, response, body) {
-	 	//   console.log(response.statusCode);
-	 	//   console.log(body);
-	 	//   batches = body;
-	 	// }
-		// );
-		return batches;
-	}
+		this.apiGetCall('https://shreddr.captricity.com/api/v1/batch/', function(body) {
+            //console.log(body);
+            return body;
+        })
+	},
+
+    /**
+     *  Get the details on a specific Batch owned by the calling user
+     */
+    readBatch: function(batchId, callback_function) {
+        var options = {
+            url: 'https://shreddr.captricity.com/api/v1/batch/' + batchId,
+            method: 'GET',
+            headers: this.headers
+        };
+        request(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                callback_function(body);
+            } else {
+                console.log('Status Code Returned:', response.statusCode);
+                console.log('Error:', error);
+            }
+        });
+    }
 };
 
 if (typeof module !== 'undefined') {
@@ -43,5 +72,17 @@ if (typeof module !== 'undefined') {
 }
 
 var runner = captools.connect(process.env.CAPTRICITY_API_TOKEN);
-console.log(runner.headers['Captricity-API-Token']);
-runner.listBatches();
+//console.log(runner.headers['Captricity-API-Token']);
+
+runner.readBatch(27962, function(response) {
+    console.log(response);
+});
+
+//runner.readBatch(29417);
+//var batchList = runner.listBatches();
+//console.log(batchList);
+//for (var i = 0, l = batchList.length; i < l; i++) {
+//    console.log(batchList[i].name);
+//    console.log(batchList[i]['submitted']);
+//    console.log("===========================");
+//}
